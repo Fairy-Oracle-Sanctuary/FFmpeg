288a3208c70f804217755f62c7282926a53de56c
baby-2016 <2185823427@qq.com>
2026-07-12 17:37:08 +0800

    fix(ci): 添加autoconf/automake/libtool依赖，修正libvpx/x265交叉编译配置

--- build-win-x64.sh
+++ build-win-x64.sh
@@ -17,7 +17,7 @@ build-dep(){
 
   git clone https://chromium.googlesource.com/webm/libvpx.git --depth 1
   cd libvpx
-  ./configure --prefix=${INSTALL_DIR} --target=x86_64-win64-gcc --enable-static --disable-shared --disable-examples --disable-tools --disable-unit-tests --disable-docs --disable-debug --cross=${CROSS_PREFIX}
+  CROSS=${CROSS_PREFIX} ./configure --prefix=${INSTALL_DIR} --target=x86_64-win64-gcc --enable-static --disable-shared --disable-examples --disable-tools --disable-unit-tests --disable-docs --disable-debug
   make -j$(nproc) install
   cd ..
 
@@ -29,6 +29,17 @@ build-dep(){
 
   git clone https://bitbucket.org/multicoreware/x265_git.git --depth 1
   cd x265_git/build/linux
+  cat > toolchain-x86_64-w64-mingw32.cmake <<'TCM'
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+set(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
+set(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+TCM
   cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DENABLE_SHARED=OFF -DENABLE_CLI=OFF -DCMAKE_TOOLCHAIN_FILE=toolchain-x86_64-w64-mingw32.cmake ../../source
   make -j$(nproc) install
   cd ../../..
